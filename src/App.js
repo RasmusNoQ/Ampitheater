aaae5037b2edef42fd140c2f286390cbf5a3f50f
Daniel Lucas <danyyel770@gmail.com>
2022-08-18 16:03:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import './App.scss';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header/>
+        <div className='container'>
+          <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/movie/:imdbID' element={<MovieDetails/>}/>
+          <Route element={<PageNotFound/>}/>
+          </Routes>
+        </div>
+        <Footer/>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
